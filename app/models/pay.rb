3be0bade07808f2f3aa5c8d383020e2382ffde76
torucom <torucom@mac.com>
2019-06-20 20:57:03 +0900

    バリデーション調整

--- app/models/pay.rb
+++ app/models/pay.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class Pay < ApplicationRecord
   belongs_to :expend
-  validates :payable, presence: true, length: { minimum: 3, message:'3文字以上で入力してください。' }
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1, message:'金額を入力し得てください。' }
+  validates :payable, presence: true
+  validates :cost, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
